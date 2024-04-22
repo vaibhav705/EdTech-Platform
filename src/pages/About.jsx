6196ae012624db1cce36b15181a31ae383f556ed
vaibhav705 <singhaniya199829@gmail.com>
2024-04-22 17:28:47 +0530

    First commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,59 @@
+import React from "react"
+
+import BannerImage1 from "../assets/Images/aboutus1.webp"
+import BannerImage2 from "../assets/Images/aboutus2.webp"
+import BannerImage3 from "../assets/Images/aboutus3.webp"
+import Footer from "../components/common/Footer"
+import ReviewSlider from "../components/common/ReviewSlider"
+import ContactFormSection from "../components/core/AboutPage/ContactFormSection"
+import Quote from "../components/core/AboutPage/Quote"
+import HighlightText from "../components/core/HomePage/HighlightText"
+
+const About = () => {
+  return (
+    <div>
+      <section className="bg-richblack-700">
+        <div className="relative mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-center text-white">
+          <header className="mx-auto py-20 text-4xl font-semibold lg:w-[70%]">
+            Driving Innovation in Online Education for a
+            <HighlightText text={"Brighter Future"} />
+            <p className="mx-auto mt-3 text-center text-base font-medium text-richblack-300 lg:w-[95%]">
+              We're passionate about creating a brighter future by
+              offering cutting-edge courses, leveraging emerging technologies,
+              and nurturing a vibrant learning community.
+            </p>
+          </header>
+          <div className="sm:h-[70px] lg:h-[150px]"></div>
+          <div className="absolute bottom-0 left-[50%] grid w-[100%] translate-x-[-50%] translate-y-[30%] grid-cols-3 gap-3 lg:gap-5">
+            <img src={BannerImage1} alt="" />
+            <img src={BannerImage2} alt="" />
+            <img src={BannerImage3} alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="border-b border-richblack-700">
+        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-richblack-500">
+          <div className="h-[100px] "></div>
+          <Quote />
+        </div>
+      </section>
+
+
+      <section className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white">
+        <ContactFormSection />
+      </section>
+
+      <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
+        {/* Reviws from Other Learner */}
+        <h1 className="text-center text-4xl font-semibold mt-8">
+          Reviews from other learners
+        </h1>
+        <ReviewSlider />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default About
